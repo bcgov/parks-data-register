afb829daeed6df2d084a2cca5412a0a7231ca90f
Daniel Truong <daniel@digitalspace.ca>
2023-09-15 08:47:48 -0700

    API-63: Adding script for initial data load (#114)
    * API-63: Adding script for initial data load
    
    * Updating code to fix brain-overload.
    
    * Fixing code smell

---
+++ pdr-api/tools/dataLoad.js
@@ -0,0 +1,98 @@
+const AWS = require('aws-sdk');
+const csv = require('csvtojson');
+const { DateTime } = require('luxon');
+
+const bcParkNamesPath = './BC Parks Names.csv';
+const parParksPath = './PAR - Parks.csv';
+
+const TABLE_NAME = process.env.TABLE_NAME || 'NameRegister';
+const TIMEZONE = 'America/Vancouver';
+
+const options = {
+  region: 'ca-central-1'
+};
+
+if (process.env.IS_OFFLINE) {
+  options.endpoint = 'http://localhost:8000';
+}
+
+const dynamodb = new AWS.DynamoDB(options);
+
+async function run() {
+  const bcParksNames = await csv().fromFile(bcParkNamesPath);
+  let parParks = await csv().fromFile(parParksPath);
+
+  console.log('Data Load');
+  console.log(`Loading ${bcParksNames.length} items.`);
+
+  const currentPSTDateTime = DateTime.now().setZone(TIMEZONE);
+  const currentTimeISO = currentPSTDateTime.toUTC().toISO();
+
+  for (let record of bcParksNames) {
+    // establishedDate:
+    const res = getEstablishedDate(record, parParks);
+    const establishedDate = res[0];
+    parParks = res[1];
+
+    // createDate:
+    // Check if record already exists
+    const existingRecord = AWS.DynamoDB.Converter.unmarshall(await getOne(record.orcs, 'Details'));
+    // Get createDate if it does
+    const createDate = existingRecord ? existingRecord.createDate : currentTimeISO;
+
+    let updateParams = {
+      TableName: TABLE_NAME,
+      Key: {
+        pk: { S: record.orcs },
+        sk: { S: 'Details' }
+      },
+      ExpressionAttributeValues: {
+        ':createDate': { S: createDate },
+        ':updateDate': { S: currentTimeISO },
+        ':effectiveDate': { S: establishedDate },
+        ':legalName': { S: record.legalName },
+        ':displayName': { S: record.displayName },
+        ':phoneticName': { S: record.phoneticName },
+        ':status': { S: '' },
+        ':notes': { S: record['validation note'] }
+      },
+      ExpressionAttributeNames: { '#status': 'status' },
+      UpdateExpression:
+        'SET createDate = :createDate, updateDate = :updateDate, effectiveDate = :effectiveDate, legalName = :legalName, displayName = :displayName, phoneticName = :phoneticName, #status = :status, notes = :notes',
+      ReturnValues: 'ALL_NEW'
+    };
+
+    try {
+      await dynamodb.updateItem(updateParams).promise();
+    } catch (error) {
+      console.log('Error:', error);
+      console.log(updateParams);
+    }
+  }
+  console.log('Data load complete.');
+}
+
+function getEstablishedDate(record, parParks) {
+  // Get established date for effective date field.
+  let establishedDate = '';
+  let i = parParks.length;
+  while (i--) {
+    if (record.orcs === parParks[i].ORCS) {
+      establishedDate = parParks[i]['Established Date'] ? parParks[i]['Established Date'] : '';
+      parParks.splice(i, 1);
+      i = 0;
+    }
+  }
+  return [establishedDate, parParks];
+}
+
+async function getOne(pk, sk) {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: AWS.DynamoDB.Converter.marshall({ pk, sk })
+  };
+  let item = await dynamodb.getItem(params).promise();
+  return item?.Item || {};
+}
+
+run();

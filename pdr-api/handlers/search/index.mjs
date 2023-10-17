b981d48db38f2463fed6d3db76dd5c402f39290b
Mark Lisé <mark@digitalspace.ca>
2023-10-16 17:03:14 -0700

    134: Adding /search/ endpoint for OpenSearch, redacting notes field if not Admin user.  Call with mandatory query param ?text=<searchString>

--- pdr-api/handlers/search/index.mjs
+++ pdr-api/handlers/search/index.mjs
@@ -1,15 +1,17 @@
+// Import necessary libraries and modules
+const OPENSEARCH_DOMAIN_ENDPOINT = process.env.OPENSEARCH_DOMAIN_ENDPOINT;
+const OPENSEARCH_MAIN_INDEX      = process.env.OPENSEARCH_MAIN_INDEX;
+
 import { defaultProvider } from '@aws-sdk/credential-provider-node'; // V3 SDK.
 import { Client } from '@opensearch-project/opensearch';
 import { AwsSigv4Signer } from '@opensearch-project/opensearch/aws';
-const OPENSEARCH_DOMAIN_ENDPOINT = process.env.OPENSEARCH_DOMAIN_ENDPOINT;
-const OPENSEARCH_MAIN_INDEX = process.env.OPENSEARCH_MAIN_INDEX
+import { sendResponse, logger } from '/opt/base.js';
 
 const client = new Client({
   ...AwsSigv4Signer({
     region: 'ca-central-1',
     service: 'es',
     getCredentials: () => {
-      // Any other method to acquire a new Credentials object can be used.
       const credentialsProvider = defaultProvider();
       return credentialsProvider();
     },
@@ -17,28 +19,51 @@ const client = new Client({
   node: OPENSEARCH_DOMAIN_ENDPOINT, // OpenSearch domain URL
 });
 
-export const handler = async (event) => {
-  console.log('Searching:');
+// Lambda function entry point
+export const handler = async (event, context) => {
+  logger.debug('Search:', event); // Log the search event
 
-  let query = {
-    query: {
-      query_string: {
-        query: 'Taku'
+  try {
+    // Extract query parameters from the event
+    const queryParams = event.queryStringParameters;
+
+    if (!queryParams?.text) {
+      logger.err(`Bad Request - Invalid Params:${queryParams}`);
+      return sendResponse(400, {}, 'Bad Request', 'Invalid Params', context);
+    }
+
+    // Check if the user is an admin
+    const isAdmin = JSON.parse(event.requestContext?.authorizer?.isAdmin || false);
+
+    // Construct the search query
+    const query = {
+      query: {
+        query_string: {
+          query: queryParams?.text,
+        },
       },
-    },
-  };
+    };
 
-  let response = await client.search({
-    index: OPENSEARCH_MAIN_INDEX,
-    body: query,
-  });
+    // Send the query to the OpenSearch cluster
+    let response = await client.search({
+      index: OPENSEARCH_MAIN_INDEX, // Index to search
+      body: query,
+    });
+    logger.debug(response); // Log the response
 
-  console.log(JSON.stringify(response.body.hits));
+    // Redact the "notes" field if the user is not an admin
+    if (!isAdmin) {
+      for (let i = 0; i < response.body.hits.total.value; i++) {
+        delete response.body.hits.hits[i]._source.notes;
+      }
+    }
 
-  // TODO implement
-  response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+    // Send a success response
+    return sendResponse(200, response.body.hits, 'Success', null, context);
+  } catch (err) {
+    logger.error(JSON.stringify(err)); // Log the error
+
+    // Send an error response
+    return sendResponse(err?.code || 400, [], err?.msg || 'Error', err?.error || err, context);
+  }
 };

9c6b359b5ea3816937677cc92f38d2365d5340f3
cameron <cameron@digitalspace.ca>
2023-10-27 12:07:42 +1400

    PDR-167 allowing policy generation for all endpoints at once

--- pdr-api/__tests__/settings.js
+++ pdr-api/__tests__/settings.js
@@ -1,11 +1,13 @@
-const REGION = 'local-env';
+const AWS_REGION = process.env.AWS_REGION || 'local-env';
 const ENDPOINT = 'http://localhost:8000';
-const TABLE_NAME = process.env.TABLE_NAME || 'NameRegister-tests';
+const DYNAMODB_ENDPOINT_URL = process.env.DYNAMODB_ENDPOINT_URL = ENDPOINT;
+const TABLE_NAME = process.env.TABLE_NAME || 'NameRegistry-tests';
 const TIMEZONE = 'America/Vancouver';
 const { DocumentClient } = require('aws-sdk/clients/dynamodb');
 const DBMODEL = require('../docs/dbModel.json');
 const AWS = require('aws-sdk');
-const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
+process.env.LOG_LEVEL = 'info';
+process.env.IS_OFFLINE = 'true';
 
 // Set the TABLE_NAME to be the test table name in the database model.
 // process.env.TABLE_NAME = TABLE_NAME;
@@ -14,7 +16,7 @@ DBMODEL.TableName = TABLE_NAME;
 async function createDB(items, tableName = TABLE_NAME) {
 
   const docClient = new DocumentClient({
-    region: REGION,
+    region: AWS_REGION,
     endpoint: ENDPOINT,
     convertEmptyValues: true
   });
@@ -35,7 +37,8 @@ async function createDB(items, tableName = TABLE_NAME) {
 
 module.exports = {
   createDB,
-  REGION,
+  DYNAMODB_ENDPOINT_URL,
+  AWS_REGION,
   ENDPOINT,
   TABLE_NAME,
   TIMEZONE,

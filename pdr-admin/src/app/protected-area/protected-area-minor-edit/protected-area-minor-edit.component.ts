69ea1ea429524b214968295f9875db0a99a63419
Daniel Truong <daniel@digitalspace.ca>
2023-11-07 17:36:45 +0000

    Removing commented out code

---
+++ pdr-admin/src/app/protected-area/protected-area-minor-edit/protected-area-minor-edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protected-area-minor-edit',
+  templateUrl: './protected-area-minor-edit.component.html',
+  styleUrls: ['./protected-area-minor-edit.component.scss'],
+})
+export class ProtectedAreaMinorEditComponent {}

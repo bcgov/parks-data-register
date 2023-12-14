695aa15e69f6d58808ca8d4b5a0dee8b8cfaa3c2
cameron <cameron@digitalspace.ca>
2023-12-15 08:23:11 +1400

    PDR-295-2 Adding loadmore button

--- pdr-admin/src/app/services/search.service.ts
+++ pdr-admin/src/app/services/search.service.ts
@@ -15,6 +15,8 @@ import { Utils } from '../utils/utils';
 export class SearchService {
   private utils = new Utils();
 
+  public pageSize = 6;
+
   constructor(
     private dataService: DataService,
     private apiService: ApiService,
@@ -22,20 +24,44 @@ export class SearchService {
     private loggerService: LoggerService,
     private toastService: ToastService,
     private eventService: EventService
-  ) {}
+  ) { }
 
-  async fetchData(queryParams, cacheUrl = null) {
+  async fetchData(queryParams, cacheUrl = null, startFrom = 0) {
     this.loadingService.addToFetchList(Constants.dataIds.SEARCH_RESULTS);
     // Calling API with status = null gives you current and historical
     try {
+      // Start from a certain value if provided
+      queryParams['startFrom'] = startFrom;
+      // Add 1 to the result limit. If the number of results returned is equal to this additional limit, we know there is at least 1 more page of results to query.
+      let lastPage = true;
+      queryParams['limit'] = this.pageSize + 1;
       const res = await lastValueFrom(this.apiService.get('search', queryParams));
       const data = this.apiService.getArrayFromSearchResults(res);
+      if (data.length >= this.pageSize + 1) {
+        // We know theres more pages of results because we were able to bring in more than the page size.
+        // Remove the last result
+        data.pop();
+        lastPage = false;
+      }
+      const lastResultIndex = startFrom + data.length;
       for (let i = 0; i < data.length; i++) {
         data[i] = this.utils.setLastVersionDate(data[i]);
       }
-      this.dataService.setItemValue(Constants.dataIds.SEARCH_RESULTS, data);
+      const searchParams = {
+        lastPage: lastPage,
+        lastResultIndex: lastResultIndex,
+        lastQuery: queryParams
+      }
+      if (startFrom) {
+        // we want to append to the previous results
+        this.dataService.appendItemValue(Constants.dataIds.SEARCH_RESULTS, data);
+      } else {
+        // we want to overwrite the previous results
+        this.dataService.setItemValue(Constants.dataIds.SEARCH_RESULTS, data);
+      }
+      this.dataService.setItemValue(Constants.dataIds.SEARCH_PARAMS, searchParams);
       if (cacheUrl) {
-        this.dataService.setCacheValue(cacheUrl, data, 300);
+        this.dataService.setCacheValue(cacheUrl, this.dataService.getItemValue(Constants.dataIds.SEARCH_RESULTS), 300);
         this.loggerService.debug(`Cache update ${cacheUrl}`);
       }
     } catch (e) {
@@ -49,11 +75,16 @@ export class SearchService {
   watchSearchResults() {
     return this.dataService.watchItem(Constants.dataIds.SEARCH_RESULTS);
   }
+
+  watchSearchParams() {
+    return this.dataService.watchItem(Constants.dataIds.SEARCH_PARAMS);
+  }
+
   clearSearchResults() {
     return this.dataService.clearItemValue(Constants.dataIds.SEARCH_RESULTS);
   }
 
-  checkCache(url){
+  checkCache(url) {
     return this.dataService.getCachedValue(url);
   }
 
--- pdr-admin/src/app/utils/constants.ts
+++ pdr-admin/src/app/utils/constants.ts
@@ -2,6 +2,7 @@ export class Constants {
   public static readonly dataIds = {
     ENTER_DATA_URL_PARAMS: 'enter-data-url-params',
     SEARCH_RESULTS: 'search-results',
+    SEARCH_PARAMS: 'search-params',
     CURRENT_PROTECTED_AREA: 'current-protected-area',
     PROTECTED_AREA_PUT: 'protected-area-put',
     HISTORICAL_PROTECTED_AREA: 'historical-protected-area',

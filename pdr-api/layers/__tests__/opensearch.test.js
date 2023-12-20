516c0acd76387f8a4b37cb4a14e817e6243cf514
cameron <cameron@digitalspace.ca>
2023-12-21 06:57:37 +1400

    PDR-230-1 moving opensearch to lambda layer

---
+++ pdr-api/layers/__tests__/opensearch.test.js
@@ -0,0 +1,244 @@
+const {
+  OPENSEARCH_DOMAIN_ENDPOINT,
+  OPENSEARCH_MAIN_INDEX,
+  OSQuery,
+} = require('../../.aws-sam/build/OpenSearchLayer/opensearch');
+
+const DEFAULT_RESULT_SIZE = 10;
+const MAX_RESULT_SIZE = 100;
+const layer = require('../../.aws-sam/build/OpenSearchLayer/opensearch');
+
+// Mocks for the environment variables
+process.env.OPENSEARCH_DOMAIN_ENDPOINT = 'http://localhost:9200';
+process.env.OPENSEARCH_MAIN_INDEX = 'main-index';
+
+describe('OSQuery class', () => {
+  let originalEnv;
+
+  beforeAll(() => {
+    originalEnv = process.env;
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    process.env = { ...originalEnv };
+  });
+
+  afterAll(() => {
+    process.env = originalEnv;
+  });
+
+  describe('Constructor', () => {
+    test('should initialize with default values when no parameters provided', async () => {
+      const queryInstance = new OSQuery();
+      expect(queryInstance.index).toEqual(OPENSEARCH_MAIN_INDEX);
+      expect(queryInstance.size).toEqual(DEFAULT_RESULT_SIZE);
+      expect(queryInstance.from).toEqual(0);
+      expect(queryInstance.sortField).toEqual(null);
+      expect(queryInstance.sortOrder).toEqual('asc');
+
+    });
+
+    test('should initialize with provided values', async () => {
+      const customIndex = 'custom-index';
+      const customSize = 20;
+      const customFrom = 5;
+      const customField = 'field';
+      const customOrder = 'desc';
+
+      const queryInstance = new OSQuery(customIndex, customSize, customFrom, customField, customOrder);
+
+      expect(queryInstance.index).toEqual(customIndex);
+      expect(queryInstance.size).toEqual(customSize);
+      expect(queryInstance.from).toEqual(customFrom);
+      expect(queryInstance.sortField).toEqual(customField);
+      expect(queryInstance.sortOrder).toEqual(customOrder);
+    });
+  });
+
+  describe('search method', () => {
+    test('should call OpenSearch client search method with correct parameters', async () => {
+
+      const queryInstance = new OSQuery();
+
+      const searchSpy = jest.spyOn(layer.client, 'search');
+
+      queryInstance.query = {
+        bool: {
+          must: {
+            query_string: {
+              query: 'test'
+            }
+          }
+        }
+      }
+
+      await queryInstance.search();
+
+      expect(searchSpy).toHaveBeenCalledWith({
+        index: queryInstance.index,
+        size: queryInstance.size,
+        from: queryInstance.from,
+        body: {
+          query: queryInstance.query,
+        },
+      });
+    });
+  });
+
+  describe('addMatchQueryStringRule method', () => {
+    test('should set match query string rule correctly', async () => {
+      const queryInstance = new OSQuery();
+
+      queryInstance.addMatchQueryStringRule('example');
+
+      expect(queryInstance.query).toEqual({
+        bool: {
+          must: [{
+            query_string: {
+              query: 'example',
+            },
+          }],
+        },
+      });
+
+      // Test escaped characters
+      queryInstance.addMatchQueryStringRule('example&query');
+
+      expect(queryInstance.query).toEqual({
+        bool: {
+          must: [
+            {
+              query_string: {
+                query: 'example',
+              },
+            },
+            {
+              query_string: {
+                query: 'example\\&query',
+              },
+            },
+          ],
+        },
+      });
+    });
+  });
+
+  describe('addMatchTermsRule method', () => {
+    test('should call addTermsRule correctly', async () => {
+      const queryInstance = new OSQuery();
+      const terms = { field: 'value' };
+      const terms2 = { field2: 'value2', field3: 'value3' };
+
+      queryInstance.addMatchTermsRule(terms, true);
+
+      expect(queryInstance.query).toEqual({
+        bool: {
+          must: [{
+            terms: {
+              field: ['value'],
+            },
+          }],
+        },
+      });
+
+      queryInstance.addMatchTermsRule(terms2, true);
+
+      expect(queryInstance.query).toEqual({
+        bool: {
+          must: [
+            {
+              terms: {
+                field: ['value'],
+              },
+            },
+            {
+              terms: {
+                field2: ['value2'],
+              },
+            },
+            {
+              terms: {
+                field3: ['value3'],
+              },
+            },
+          ],
+        },
+      });
+    });
+  });
+
+  describe('addIgnoreTermsRule method', () => {
+    test('should call addTermsRule correctly with ignore=true and exactMatch=false', async () => {
+      const queryInstance = new OSQuery();
+      const terms = { field: 'value' };
+      const terms2 = { field2: 'value2', field3: 'value3' };
+
+      queryInstance.addIgnoreTermsRule(terms);
+
+      expect(queryInstance.query).toEqual({
+        bool: {
+          must_not: [{
+            match: {
+              field: 'value',
+            },
+          }],
+        },
+      });
+
+      queryInstance.addIgnoreTermsRule(terms2);
+
+      expect(queryInstance.query).toEqual({
+        bool: {
+          must_not: [
+            {
+              match: {
+                field: 'value',
+              },
+            },
+            {
+              match: {
+                field2: 'value2',
+              },
+            },
+            {
+              match: {
+                field3: 'value3',
+              },
+            },
+          ],
+        },
+      });
+    });
+  });
+});
+
+describe('addSortRule function', () => {
+  test('should add sort rules correctly', async () => {
+    const sortRule = new OSQuery();
+    sortRule.addSortRule('field');
+    expect(sortRule.sort).toEqual([{
+      'field': {
+        order: 'asc'
+      }
+    }]);
+  })
+})
+
+describe('setSearchLimit function', () => {
+  test('should return the adjusted search limit within allowed bounds', async () => {
+    const limit = 150;
+
+    const adjustedLimit = new OSQuery('index', limit);
+
+    expect(adjustedLimit.size).toEqual(MAX_RESULT_SIZE);
+  });
+
+  test('should return 1 when requested limit is less than 1', async () => {
+    const limit = -5;
+
+    const adjustedLimit = new OSQuery('index', limit);
+
+    expect(adjustedLimit.size).toEqual(1);
+  });
+});
---
+++ pdr-api/layers/opensearch/opensearch.js
@@ -0,0 +1,273 @@
+
+const { defaultProvider } = require('@aws-sdk/credential-provider-node'); // V3 SDK.
+const { Client } = require('@opensearch-project/opensearch');
+const { AwsSigv4Signer } = require('@opensearch-project/opensearch/aws');
+
+// Import necessary libraries and modules
+const OPENSEARCH_DOMAIN_ENDPOINT = process.env.OPENSEARCH_DOMAIN_ENDPOINT || 'http://localhost:9200';
+const OPENSEARCH_MAIN_INDEX = process.env.OPENSEARCH_MAIN_INDEX || 'main-index';
+const OPENSEARCH_AUDIT_INDEX = process.env.OPENSEARCH_MAIN_INDEX || 'audit-index';
+const OPENSEARCH_DEFAULT_SORT_ORDER = 'asc'; // asc or desc
+const DEFAULT_RESULT_SIZE = 10;
+const MAX_RESULT_SIZE = 100;
+// Query parameters that should not be used as keyable search terms
+const nonKeyableTerms = [
+  'text',
+  'startFrom',
+  'limit',
+  'sortField',
+  'sortOrder',
+]
+
+let client = new Client({
+  ...AwsSigv4Signer({
+    region: 'ca-central-1',
+    service: 'es',
+    getCredentials: () => {
+      const credentialsProvider = defaultProvider();
+      return credentialsProvider();
+    }
+  }),
+  node: OPENSEARCH_DOMAIN_ENDPOINT // OpenSearch domain URL
+});
+
+// For offline development
+if (process.env.IS_OFFLINE === 'true') {
+  client = new Client({
+    node: OPENSEARCH_DOMAIN_ENDPOINT,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+}
+
+/**
+ * A class representing an OpenSearch query builder.
+ *
+ * @class
+ */
+class OSQuery {
+  /**
+   * Creates an instance of OSQuery.
+   *
+   * @constructor
+   * @param {string} [index=OPENSEARCH_MAIN_INDEX] - The index to search.
+   * @param {number} [size=setSearchLimit(size)] - The maximum number of results to return.
+   * @param {number} [from=0] - The starting index for the results.
+   * @param {number} [sortField=null] - A field to sort the results by.
+   * @param {number} [sortOrder=OPENSEARCH_DEFAULT_SORT_ORDER] - The sorting method to use when sorting results.
+   */
+  constructor(index, size, from, sortField, sortOrder) {
+    /**
+     * The query object representing the OpenSearch query.
+     * @type {Object}
+     */
+    this.query = {};
+
+    /**
+     * The sort object for sorting the results.
+     * @type {Object}
+     */
+    this.sort = {};
+
+    /**
+     * The index to search.
+     * @type {string}
+     */
+    this.index = index || OPENSEARCH_MAIN_INDEX;
+    /**
+     * The maximum number of results to return.
+     * @type {number}
+     */
+    this.size = setSearchLimit(size);
+    /**
+     * The starting index for the results.
+     * @type {number}
+     */
+    this.from = from || 0;
+    this.sortField = sortField || null;
+    this.sortOrder = sortOrder || OPENSEARCH_DEFAULT_SORT_ORDER;
+    this.sort = this.initSortQuery();
+    this.request = null;
+  }
+
+  /**
+   * Performs an asynchronous search using the configured query parameters.
+   *
+   * @async
+   * @returns {Promise<Object>} A Promise that resolves to the search result.
+   */
+  async search() {
+    this.request = {
+      index: this.index,
+      size: this.size,
+      from: this.from,
+      body: {
+        query: this.query,
+      }
+    }
+    if (this.sort) {
+      this.request.body['sort'] = this.sort;
+    }
+    return await client.search(this.request);
+  }
+
+  initSortQuery() {
+    if (this.sortField) {
+      this.sort = [
+        {
+          [this.sortField]: {
+            order: this.sortOrder
+          }
+        }
+      ]
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Adds a match query string rule to the OpenSearch query.
+   *
+   * @param {string} string - The string to match in the query.
+   */
+  addMatchQueryStringRule(string) {
+    setNestedValue(
+      this.query,
+      ['bool', 'must'],
+      {
+        query_string: {
+          query: escapeOpenSearchQuery(string)
+        }
+      }
+    )
+  }
+
+  /**
+   * Adds match terms rule to the OpenSearch query.
+   *
+   * @param {Array} terms - An array of terms to match in the query.
+   * @param {Boolean} exactMatch - If true, term must match exactly to return a hit. Default false. 
+   */
+  addMatchTermsRule(terms, exactMatch = false) {
+    addTermsRule(this.query, terms, false, exactMatch)
+  }
+
+  /**
+   * Adds ignore terms rule to the OpenSearch query.
+   *
+   * @param {Array} terms - An array of terms to exclude from the query.
+   * @param {Boolean} exactMatch - If true, term must match exactly to ignore a hit. Default false. 
+   */
+  addIgnoreTermsRule(terms, exactMatch = false) {
+    addTermsRule(this.query, terms, true, exactMatch)
+  }
+
+  /**
+   * Adds sort rule to the OpenSearch query. Only single field sorting is currently supported
+   *
+   * @param {String} field - The field to order by
+   * @param {String} order - Sort order ('asc' or 'desc').
+   */
+  addSortRule(field, order = OPENSEARCH_DEFAULT_SORT_ORDER) {
+    this.sort = [{
+      [field]: {
+        order: order
+      }
+    }]
+  }
+
+}
+
+/**
+ * Adds terms rule to the OpenSearch query.
+ *
+ * @param {Object} query - The OpenSearch query object to which the terms rule will be added.
+ * @param {Object} terms - An object representing the terms to be matched in the query.
+ * @param {boolean} [ignore=false] - If true, adds terms as "must_not" in the query; otherwise, adds as "must".
+ * @param {boolean} [exactMatch=true] - If true, uses "terms" in the match; otherwise, uses "match".
+ */
+function addTermsRule(query, terms, ignore = false, exactMatch = true) {
+  const must = ignore ? 'must_not' : 'must';
+  const match = exactMatch ? 'terms' : 'match';
+  for (const term of Object.keys(terms)) {
+    let value = terms[term].toLowerCase();
+    if (exactMatch) {
+      value = value.split(',')
+    }
+    setNestedValue(
+      query,
+      ['bool', must],
+      {
+        [match]: {
+          [term]: value
+        }
+      }
+    )
+  }
+}
+
+/**
+ * Sets a nested value in an object based on an array of keys.
+ *
+ * @param {Object} root - The root object in which the nested value will be set.
+ * @param {Array} keys - An array of keys representing the path to the nested value.
+ * @param {any} value - The value to be set at the nested path.
+ * @param {boolean} [append=true] - If true, appends the value to an existing array; otherwise, replaces the value.
+ */
+function setNestedValue(root, keys, value, append = true) {
+  if (keys.length === 1) {
+    if (append) {
+      if (root?.[keys[0]]?.length) {
+        root[keys[0]].push(value);
+      } else {
+        root[keys[0]] = [value];
+      }
+    } else {
+      root[keys[0]] = value;
+    }
+  } else {
+    root[keys[0]] = root[keys[0]] || {};
+    setNestedValue(root[keys[0]], keys.slice(1), value);
+  }
+}
+
+/**
+ * Escapes special characters in an OpenSearch query string.
+ *
+ * @param {string} input - The input string to be escaped.
+ * @returns {string} The escaped OpenSearch query string.
+ */
+function escapeOpenSearchQuery(input) {
+  // Define a regular expression pattern for reserved characters
+  const pattern = /([-&|!(){}[\]^"~*?:\/+])/g;
+  // Use the replace method with a callback function to handle replacements
+  const escapedQuery = input.replace(pattern, (match, p1) => `\\${p1}`);
+  return escapedQuery;
+}
+
+/**
+ * Sets the search limit, ensuring it falls within the specified bounds.
+ *
+ * @param {number} [limit=DEFAULT_RESULT_SIZE] - The requested limit for search results.
+ * @returns {number} The adjusted search limit within the allowed bounds.
+ */
+function setSearchLimit(limit = DEFAULT_RESULT_SIZE) {
+  let requestedLimit = limit;
+  if (requestedLimit < 1) {
+    requestedLimit = 1;
+  }
+  if (requestedLimit > MAX_RESULT_SIZE) {
+    requestedLimit = MAX_RESULT_SIZE
+  }
+  return requestedLimit;
+}
+
+module.exports = {
+  OPENSEARCH_DOMAIN_ENDPOINT,
+  OPENSEARCH_MAIN_INDEX,
+  OPENSEARCH_AUDIT_INDEX,
+  nonKeyableTerms,
+  client,
+  OSQuery
+}

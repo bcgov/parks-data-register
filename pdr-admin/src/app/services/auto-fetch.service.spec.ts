1d3df11c427731c9ae14d160d0ea46d830b90fac
Daniel Truong <daniel@digitalspace.ca>
2024-01-08 16:30:27 -0800

    PDR-291: Added UTs. (#327)

--- pdr-admin/src/app/services/auto-fetch.service.spec.ts
+++ pdr-admin/src/app/services/auto-fetch.service.spec.ts
@@ -48,14 +48,51 @@ describe('AutoFetchService', () => {
     expect(service.fetchQueue).toBeDefined();
   });
 
-  // it('fetches the queue', async () => {
-  //   const fetchSpy = spyOn(service, 'runFetches').and.callThrough();
-  //   const parkSpy = spyOn(service['parkService'], 'fetchData');
-  //   await service.run();
-  //   expect(fetchSpy).toHaveBeenCalledTimes(1);
-  //   expect(parkSpy).toHaveBeenCalledTimes(1);
-  //   jasmine.clock().tick(1001);
-  //   expect(fetchSpy).toHaveBeenCalledTimes(2);
-  //   expect(parkSpy).toHaveBeenCalledTimes(2);
-  // });
+  it('fetches the queue', async () => {
+    const fetchSpy = spyOn(service, 'runFetches').and.callThrough();
+    await service.run();
+    expect(fetchSpy).toHaveBeenCalledTimes(1);
+    jasmine.clock().tick(1001);
+    expect(fetchSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('ignores order compare: Both inputs must be arrays.', async () => {
+    const loggerSpy = spyOn(mockLoggerService, 'debug').and.callThrough();
+    const res = service.ignoreOrderCompare([], 42);
+    expect(loggerSpy).toHaveBeenCalledOnceWith('Both inputs must be arrays.');
+    expect(res).toEqual(false);
+  });
+
+  it('ignores order compare: Arrays have different sizes.', async () => {
+    const loggerSpy = spyOn(mockLoggerService, 'debug').and.callThrough();
+    const res = service.ignoreOrderCompare([], [{}]);
+    expect(loggerSpy).toHaveBeenCalledOnceWith('Arrays have different sizes.');
+    expect(res).toEqual(false);
+  });
+
+  it('ignores order compare: Arrays have different sizes.', async () => {
+    const loggerSpy = spyOn(mockLoggerService, 'debug').and.callThrough();
+    const res = service.ignoreOrderCompare([], [{}]);
+    expect(loggerSpy).toHaveBeenCalledOnceWith('Arrays have different sizes.');
+    expect(res).toEqual(false);
+  });
+
+  it('ignores order compare: Object element count is different.', async () => {
+    const loggerSpy = spyOn(mockLoggerService, 'debug').and.callThrough();
+    const res = service.ignoreOrderCompare([{ element: 1 }], [{}]);
+    expect(loggerSpy).toHaveBeenCalledOnceWith('Object not found or has a different count: {}');
+    expect(res).toEqual(false);
+  });
+
+  it('ignores order compare: Different values.', async () => {
+    const loggerSpy = spyOn(mockLoggerService, 'debug').and.callThrough();
+    const res = service.ignoreOrderCompare([{ element: 1 }], [{ element: 2 }]);
+    expect(loggerSpy).toHaveBeenCalledOnceWith('Object not found or has a different count: {"element":2}');
+    expect(res).toEqual(false);
+  });
+
+  it('ignores order compare: success.', async () => {
+    const res = service.ignoreOrderCompare([{ element: 1 }, { element2: 2 }], [{ element: 1 }, { element2: 2 }]);
+    expect(res).toEqual(true);
+  });
 });
---
+++ pdr-admin/src/app/utils/utils.spec.ts
@@ -0,0 +1,72 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+
+import { Utils } from './utils';
+
+describe('Utils', () => {
+  let utils;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler],
+    });
+    utils = new Utils();
+  });
+
+  it('should be created', () => {
+    expect(utils).toBeTruthy();
+  });
+
+  it('should format date for display: returns null', () => {
+    const res = utils.formatDateForDisplay(null);
+    expect(res).toEqual(null);
+  });
+
+  it('should format date for display: success', () => {
+    const date = new Date(1989, 3, 5);
+    const res = utils.formatDateForDisplay(date);
+    expect(res).toEqual('April 04, 1989');
+  });
+
+  it('should clean put object', () => {
+    const validAttributes = ['hotdog', 'burger'];
+    const putObj = {
+      hotdog: 'yes',
+      burger: 'yup',
+      sushi: 'nope',
+    };
+    const res = utils.cleanPutObject(putObj, validAttributes);
+    expect(res).toEqual({
+      hotdog: 'yes',
+      burger: 'yup',
+    });
+  });
+
+  it('should show object is not empty', () => {
+    const res = utils.isObjEmpty({ notEmpty: 'its not!' });
+    expect(res).toEqual(false);
+  });
+
+  it('should show object is empty', () => {
+    const res = utils.isObjEmpty({});
+    expect(res).toEqual(true);
+  });
+
+  it('should make first char upper case', () => {
+    const res = utils.upperCaseFirstChar('string');
+    expect(res).toEqual('String');
+  });
+
+  it('should set last version date', () => {
+    const obj = {
+      updateDate: new Date(1989, 3, 5),
+    };
+    const res = utils.setLastVersionDate(obj);
+    expect(res.lastVersionDate).toEqual(new Date(1989, 3, 5));
+  });
+
+  it('should set display date', () => {
+    const obj = { hotdog: new Date(1989, 3, 5) };
+    const res = utils.setDisplayDate(obj, 'hotdog');
+    expect(res.hotdogDisplay).toEqual('April 04, 1989');
+  });
+});

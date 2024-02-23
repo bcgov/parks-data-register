136970c685049f080ef3835e4a7239e653c60636
Daniel Truong <daniel@digitalspace.ca>
2024-02-23 15:51:13 -0800

    Update env.js

--- pdr-admin/src/env.js
+++ pdr-admin/src/env.js
@@ -9,14 +9,14 @@
   // Environment name
   window.__env.ENVIRONMENT = "local"; // local | dev | test | prod
 
-  window.__env.API_LOCATION = "https://zloys5cfvf.execute-api.ca-central-1.amazonaws.com";
+  window.__env.API_LOCATION = "http://localhost:3000";
   window.__env.API_PATH = "/api";
   window.__env.GH_HASH = "local-build";
   window.__env.KEYCLOAK_CLIENT_ID = "data-register";
   window.__env.KEYCLOAK_URL = "https://dev.loginproxy.gov.bc.ca/auth";
   window.__env.KEYCLOAK_REALM = "bcparks-service-transformation";
   window.__env.KEYCLOAK_ENABLED = true;
-  window.__env.API_KEY = "GsVlO8cCiHdhvmigTJLGa7QLibEH6Av72tLZy7K3";
+  window.__env.API_KEY = "CHANGE-ME";
 
   // Add any feature-toggles
   // window.__env.coolFeatureActive = false;
